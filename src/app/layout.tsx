8ea0ba8e85b2cdd1eceb111508cdf44fbfdb934a
bayu-aji-nugroho <bayuastudies@gmail.com>
2025-01-24 18:48:10 +0700

    add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Bayu Aji Nugroho</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-darkMode`}
       >
